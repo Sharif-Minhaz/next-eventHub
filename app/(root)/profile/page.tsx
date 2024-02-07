5ba2b07fe121797387baae648352881c61ad5a3f
Minhaz <sharif35-3001@diu.edu.bd>
2024-02-07 17:14:07 +0600

    feat: filters, search and pagination added

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,8 +1,8 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
-// import { getOrdersByUser } from "@/lib/actions/order.actions";
-// import { IOrder } from "@/lib/database/models/order.model";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
 import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
@@ -15,9 +15,9 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 	const ordersPage = Number(searchParams?.ordersPage) || 1;
 	const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-	// const orders = await getOrdersByUser({ userId, page: ordersPage });
+	const orders = await getOrdersByUser({ userId, page: ordersPage });
 
-	// const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+	const orderedEvents = orders?.data?.map((order: IOrder) => order.event) || [];
 	const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
 	return (
@@ -34,14 +34,14 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 
 			<section className="wrapper my-8">
 				<Collection
-					data={[]}
+					data={orderedEvents}
 					emptyTitle="No event tickets purchased yet"
 					emptyStateSubtext="No worries - plenty of exciting events to explore!"
 					collectionType="My_Tickets"
-					limit={3}
+					limit={6}
 					page={ordersPage}
 					urlParamName="ordersPage"
-					totalPages={1}
+					totalPages={orders?.totalPages}
 				/>
 			</section>
 
@@ -61,7 +61,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 					emptyTitle="No events have been created yet"
 					emptyStateSubtext="Go create some now"
 					collectionType="Events_Organized"
-					limit={3}
+					limit={6}
 					page={eventsPage}
 					urlParamName="eventsPage"
 					totalPages={organizedEvents?.totalPages}
